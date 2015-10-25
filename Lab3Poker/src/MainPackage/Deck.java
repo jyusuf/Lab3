fb977d487987ded538f68ed1385fb6f77d6d2b5d
Jama Yusuf <jyusuf@udel.edu>
2015-10-25 15:21:48 -0400

    lab 3 poker

---
+++ Lab3Poker/src/MainPackage/Deck.java
@@ -0,0 +1,112 @@
+package MainPackage;
+
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class Deck {
+	
+	@XmlElement (name="Remaining Card")
+	private ArrayList<Card> cards;
+	private int numOfJokers = 2;
+	private Card Joker;
+
+	public Deck (int numOfjokers){
+		numOfJokers = numOfjokers;
+		
+		ArrayList<Card> MakingDeck = new ArrayList<Card>();
+		for (short i = 0; i <= 3; i++) {
+			eSuit SuitValue = eSuit.values()[i];			
+			for (short j = 0; j <= 12; j++) {
+				eRank RankValue = eRank.values()[j];				
+				Card NewCard = new Card(SuitValue,RankValue, (13 * i) + j+1);
+				MakingDeck.add(NewCard);
+			}
+	}
+		for (short i = 53; i< 53 + numOfJokers ; i++){
+			Card Joker = new Card(eSuit.JOKER, eRank.JOKER, i);
+			Joker.Wild = true;
+			MakingDeck.add(Joker);
+		}
+		
+	}
+	
+	
+	public Deck() {
+
+		//	Create an ArrayList of Cards, add each card
+		ArrayList<Card> MakingDeck = new ArrayList<Card>();
+		for (short i = 0; i <= 3; i++) {
+			eSuit SuitValue = eSuit.values()[i];			
+			for (short j = 0; j <= 12; j++) {
+				eRank RankValue = eRank.values()[j];				
+				Card NewCard = new Card(SuitValue,RankValue, (13 * i) + j+1);
+				MakingDeck.add(NewCard);
+			}
+		}
+		
+		for (short i = 53; i< 53 + numOfJokers ; i++){
+			Card Joker = new Card(eSuit.JOKER, eRank.JOKER, i);
+			Joker.Wild = true;
+			MakingDeck.add(Joker);
+			
+		}
+		
+		
+		//	Set the instance variable
+		cards = MakingDeck;
+		ShuffleCards();
+
+	}
+	
+
+	
+	private void ShuffleCards()
+	{
+		//	Shuffle the cards
+		Collections.shuffle(cards);
+	}
+
+	public Card drawFromDeck() {
+		// Removes the first card from the deck and return the card
+		Card FirstCard = cards.get(0);
+		cards.remove(0);
+		return FirstCard;
+	}
+
+	public int getTotalCards() {
+		// Returns the total number of cards still in the deck
+		return cards.size();
+	}
+	
+	public ArrayList<Card> getCards()
+	{
+		return this.cards;
+	}
+	
+	public StringWriter SerializeMe()
+	{
+	    StringWriter sw = new StringWriter();
+		try
+		{
+		    //Write it
+		    JAXBContext ctx = JAXBContext.newInstance(Deck.class);
+		    Marshaller m = ctx.createMarshaller();
+		    m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+		    m.marshal(this, sw);
+		    sw.close();			
+		}
+		catch (Exception ex)
+		{
+			
+		}
+    
+    return sw;
+	}
+}
